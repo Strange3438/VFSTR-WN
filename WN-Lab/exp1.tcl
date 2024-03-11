d250921e5fcb1b892dc91afbcb7e471c59febcf2
Rahul Pragada <117922719+Strange3438@users.noreply.github.com>
2024-03-11 09:50:26 +0530

    Update exp1.tcl

--- WN-Lab/exp1.tcl
+++ WN-Lab/exp1.tcl
@@ -1,13 +1,14 @@
 set ns [new Simulator]
 set nf [open out.nam w]
+
 $ns namtrace-all $nf
 
 proc finish { } {
-global ns nf
-$ns flush-trace
-close $nf
-exec nam out.nam &
-exit 0
+  global ns nf
+  $ns flush-trace
+  close $nf
+  exec nam out.nam &
+  exit 0
 }
 
 set n0 [$ns node]
